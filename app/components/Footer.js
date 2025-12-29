ffe535a3690fdf818a2f28074b116d17a48e3651
unknown <you@example.com>
2025-12-29 12:35:27 +0500

    changes

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -5,9 +5,10 @@ import { FaFacebookF, FaTwitter, FaLinkedinIn, FaInstagram } from "react-icons/f
 export default function Footer() {
   return (
     <footer>
-    
+  
+   
 
-      {/* Bottom Bar */}
+  
       <div className="bg-gray-900 text-white border-t border-gray-800 mt-12 py-6 text-center text-sm">
         &copy; {new Date().getFullYear()} Bitlink. All rights reserved.
       </div>
