a667dc2337fdd4a6067fca543100e784b96b8472
unknown <you@example.com>
2025-12-29 12:59:42 +0500

    new change

--- app/layout.js
+++ app/layout.js
@@ -25,7 +25,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-purple-50 dark:text-white`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-purple-50`}>
         <Navbar />
         {children}
         <Footer/>
